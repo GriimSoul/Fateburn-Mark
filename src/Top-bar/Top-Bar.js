35e32e970ee6c552c479212f43f8e46e54b756aa
GriimSoul <169006929+GriimSoul@users.noreply.github.com>
2025-04-23 18:15:45 -0600

    add click on logo to exit post functionality

--- src/Top-bar/Top-Bar.js
+++ src/Top-bar/Top-Bar.js
@@ -2,14 +2,30 @@ import React from 'react';
 import Search from './Search-Function/Search';
 import './Top-Bar.css';
 import logo from './fateburn.svg';
+import { exitPost } from '../Main-Area/Post-List/Post-List-Slice';
+import { clearComments } from '../Main-Area/Comments/Comments-Slice';
+import { useDispatch } from 'react-redux';
 
 function TopBar() {
+const dispatch = useDispatch();
+const inPost = useSelector(state => state.posts.inPost);
+
+function exitPost() {
+    dispatch(exitPost());
+    dispatch(clearComments());
+}
 
 return  (  <section class="topArea">
-        <div class='fateburnContainer'>
+        {inPost ? 
+        (<div class='fateburnContainer' id='ClickMe' onClick={exitPost}>
             <img src={logo} id='Logo' alt='Logo'/>
             <p id='SiteTitle'>Fateburn Mark</p>
-        </div>
+        </div>)
+        : 
+        (<div class='fateburnContainer'>
+            <img src={logo} id='Logo' alt='Logo'/>
+            <p id='SiteTitle'>Fateburn Mark</p>
+        </div>)}
         <Search/>
     </section>)
 }
